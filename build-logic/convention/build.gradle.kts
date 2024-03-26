2195ee93c79cc3ad9520e51875e8b8825d284cb9
Philipp Lackner <mail@pl-coding.com>
2024-03-26 16:42:04 +0100

    Implement AndroidFeatureUiConventionPlugin

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -31,5 +31,9 @@ gradlePlugin {
             id = "runique.android.library.compose"
             implementationClass = "AndroidLibraryComposeConventionPlugin"
         }
+        register("androidFeatureUi") {
+            id = "runique.android.feature.ui"
+            implementationClass = "AndroidFeatureUiConventionPlugin"
+        }
     }
 }
---
+++ build-logic/convention/src/main/java/com/plcoding/convention/ComposeDependencies.kt
@@ -0,0 +1,15 @@
+package com.plcoding.convention
+
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.DependencyHandlerScope
+import org.gradle.kotlin.dsl.project
+
+fun DependencyHandlerScope.addUiLayerDependencies(project: Project) {
+    "implementation"(project(":core:presentation:ui"))
+    "implementation"(project(":core:presentation:designsystem"))
+
+    "implementation"(project.libs.findBundle("koin.compose").get())
+    "implementation"(project.libs.findBundle("compose").get())
+    "debugImplementation"(project.libs.findBundle("compose.debug").get())
+    "androidTestImplementation"(project.libs.findLibrary("androidx.compose.ui.test.junit4").get())
+}
