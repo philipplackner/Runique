4565007edbb569cf36067acf3ae95d3480a928f9
Philipp Lackner <mail@pl-coding.com>
2024-03-31 18:59:35 +0200

    Implement the timer logic

---
+++ core/domain/src/main/java/com/plcoding/core/domain/Timer.kt
@@ -0,0 +1,23 @@
+package com.plcoding.core.domain
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+
+object Timer {
+
+    fun timeAndEmit(): Flow<Duration> {
+        return flow {
+            var lastEmitTime = System.currentTimeMillis()
+            while(true) {
+                delay(200L)
+                val currentTime = System.currentTimeMillis()
+                val elapsedTime = currentTime - lastEmitTime
+                emit(elapsedTime.milliseconds)
+                lastEmitTime = currentTime
+            }
+        }
+    }
+}
