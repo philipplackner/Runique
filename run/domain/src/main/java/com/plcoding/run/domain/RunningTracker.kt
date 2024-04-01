39e76f6304eb7da1aee95814f2cd07ccb7554aea
Philipp Lackner <mail@pl-coding.com>
2024-04-01 12:46:59 +0200

    Fixing polyline bug

--- run/domain/src/main/java/com/plcoding/run/domain/RunningTracker.kt
+++ run/domain/src/main/java/com/plcoding/run/domain/RunningTracker.kt
@@ -51,6 +51,17 @@ class RunningTracker(
 
     init {
         isTracking
+            .onEach { isTracking ->
+                if(!isTracking) {
+                    val newList = buildList {
+                        addAll(runData.value.locations)
+                        add(emptyList<LocationTimestamp>())
+                    }.toList()
+                    _runData.update { it.copy(
+                        locations = newList
+                    ) }
+                }
+            }
             .flatMapLatest { isTracking ->
                 if(isTracking) {
                     Timer.timeAndEmit()
