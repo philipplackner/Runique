b15b9b6700fb7f4995e783fb2dde836c4519c5c9
ssarki <sahancansarki@gmail.com>
2024-05-02 23:20:41 +0300

    Create auth, run, core modules

---
+++ auth/domain/src/main/java/com/plcoding/auth/domain/MyClass.kt
@@ -0,0 +1,4 @@
+package com.plcoding.auth.domain
+
+class MyClass {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,16 @@ dependencyResolutionManagement {
 
 rootProject.name = "Runique"
 include(":app")
- 
+include(":auth:data")
+include(":auth:domain")
+include(":auth:presentation")
+include(":core:presentation:designsystem")
+include(":core:presentation:ui")
+include(":core:domain")
+include(":core:data")
+include(":core:database")
+include(":run:data")
+include(":run:domain")
+include(":run:presentation")
+include(":run:location")
+include(":run:network")
