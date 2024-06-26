53339ff804c267efb27e13fde13e1894bd0b0e15
Philipp Lackner <mail@pl-coding.com>
2024-06-26 16:41:37 +0200

    Write first unit test

--- auth/domain/src/test/kotlin/com/plcoding/auth/domain/UserDataValidatorTest.kt
+++ auth/domain/src/test/kotlin/com/plcoding/auth/domain/UserDataValidatorTest.kt
@@ -2,12 +2,38 @@ package com.plcoding.auth.domain
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import assertk.assertions.isTrue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class UserDataValidatorTest {
 
-    @Test
-    fun testAddingTwoNumbers() {
-        assertThat(2 + 2).isEqualTo(4)
+    private lateinit var userDataValidator: UserDataValidator
+
+    @BeforeEach
+    fun setUp() {
+        userDataValidator = UserDataValidator(
+            patternValidator = object : PatternValidator {
+                override fun matches(value: String): Boolean {
+                    return true
+                }
+            }
+        )
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "Test12345, true",
+        "test12345, false",
+        "12345, false",
+        "Test-1234, true",
+        "TEST12345, false",
+    )
+    fun testValidatePassword(password: String, expectedIsValid: Boolean) {
+        val state = userDataValidator.validatePassword(password)
+
+        assertThat(state.isValidPassword).isEqualTo(expectedIsValid)
     }
 }
