dc2bcacc667d4eec81924e8f18ed8c65fb80bef9
Kai-Cilliers <kaicilliers@protonmail.com>
2024-11-29 14:51:02 +0200

    Fix - missing RunOverviewViewModel DI definition

---
+++ run/presentation/src/main/java/com/plcoding/run/presentation/run_overview/di/RunViewModelModule.kt
@@ -0,0 +1,9 @@
+package com.plcoding.run.presentation.run_overview.di
+
+import com.plcoding.run.presentation.run_overview.RunOverviewViewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.dsl.module
+
+val runViewModelModule = module {
+    viewModelOf(::RunOverviewViewModel)
+}
