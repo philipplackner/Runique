2b6d7a1d952d3a8e5dff1b004433ac19de4cd850
Kai-Cilliers <kaicilliers@protonmail.com>
2024-11-29 14:26:39 +0200

    Fix - add missing BOM implementation

--- run/location/build.gradle.kts
+++ run/location/build.gradle.kts
@@ -9,6 +9,7 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
 
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.ui.graphics)
