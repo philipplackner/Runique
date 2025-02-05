e3de60a8afa1fd9c61b54662648044d2ef892201
Kai Cilliers <kaicilliers@protonmail.com>
2025-02-05 16:04:52 +0200

    replace deprecated kotlinOptions

--- build-logic/convention/src/main/java/com/plcoding/convention/Kotlin.kt
+++ build-logic/convention/src/main/java/com/plcoding/convention/Kotlin.kt
@@ -7,6 +7,7 @@ import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 internal fun Project.configureKotlinAndroid(
@@ -41,8 +42,8 @@ internal fun Project.configureKotlinJvm() {
 
 private fun Project.configureKotlin() {
     tasks.withType<KotlinCompile>().configureEach {
-        kotlinOptions {
-            jvmTarget = JavaVersion.VERSION_11.toString()
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
         }
     }
 }
